16dbb7d42cb2e79aed0f8fe07b79e97372d588e0
Eric <ultimate.evil@gmail.com>
2013-08-19 12:14:31 -0400

    Create options.js

---
+++ javascript/options.js
@@ -0,0 +1,7 @@
+
+var optionsPage = null;
+
+goog.events.listen(window, 'load', function() {
+	optionsPage = new mbrio.OptionsPage();
+});
+//document.getElementById("#buttons").addEventListener("click",optionsPage.saveOptions());
