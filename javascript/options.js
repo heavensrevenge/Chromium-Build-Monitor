6a5fec10d642d9ea5af9354461bb5633ddf6640a
Eric <ultimate.evil@gmail.com>
2013-08-19 16:08:50 -0400

    Update options.js

--- javascript/options.js
+++ javascript/options.js
@@ -1,5 +1,5 @@
 var optionsPage = null;
 goog.events.listen(window, 'load', function() {
 	optionsPage = new mbrio.OptionsPage();
-	document.getElementById("saveButton").addEventListener("click", (function() {optionsPage.saveOptions()}), false);
+	document.getElementById("saveButton").addEventListener("click", (function() {optionsPage.saveOptions();}), false);
 });
