a1451036af42e19ab4ff43fead9a7935d6ecebf1
Eric <ultimate.evil@gmail.com>
2013-08-19 23:59:20 -0400

    Forgot to add the optionsPage.platformUpdated() EventListener so zip download isn't an option for platforms other than windows since it is special cased for Windows only

--- javascript/options.js
+++ javascript/options.js
@@ -1,5 +1,6 @@
 var optionsPage = null;
 goog.events.listen(window, 'load', function() {
 	optionsPage = new mbrio.OptionsPage();
+	document.getElementById("platform").addEventListener("change", (function() {optionsPage.platformUpdated();}), false);
 	document.getElementById("saveButton").addEventListener("click", (function() {optionsPage.saveOptions();}), false);
 });
