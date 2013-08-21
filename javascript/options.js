0adf1c49680ceceb839cf52287482293ea2f9111
Eric <ultimate.evil@gmail.com>
2013-08-21 04:45:56 -0400

    Wrapped into a function

--- javascript/options.js
+++ javascript/options.js
@@ -1,6 +1,8 @@
-var optionsPage = null;
-goog.events.listen(window, 'load', function() {
-	optionsPage = new mbrio.OptionsPage();
-	document.getElementById("platform").addEventListener("change", (function() {optionsPage.platformUpdated();}), false);
-	document.getElementById("saveButton").addEventListener("click", (function() {optionsPage.saveOptions();}), false);
-});
+(function(){
+    var optionsPage = null;
+    goog.events.listen(window, 'load', function() {
+        optionsPage = new mbrio.OptionsPage();
+        document.getElementById("platform").addEventListener("change", (function() {optionsPage.platformUpdated()}), false);
+        document.getElementById("saveButton").addEventListener("click", (function() {optionsPage.saveOptions()}), false);
+    });
+})();
