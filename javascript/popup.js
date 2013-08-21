97db14bee2ff746b0f0952c233bda4bb7076e578
Eric <ultimate.evil@gmail.com>
2013-08-21 04:45:24 -0400

    Wrapped into a function

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,8 +1,8 @@
-var snapshotPopup = null;
-goog.events.listen(window, 'load', function() {
-	snapshotPopup = new mbrio.SnapshotPopup();
-	document.getElementById("download").addEventListener("click", (function() {
-		snapshotPopup.recordDownload(chrome.extension.getBackgroundPage().snapshot.changeLogRevision);
-	}), false);
-	document.getElementById("refresh").addEventListener("click", (function() {snapshotPopup.refresh()}), false);
-});
+(function(){
+    var snapshotPopup = null;
+    goog.events.listen(window, 'load', function() {
+        snapshotPopup = new mbrio.SnapshotPopup();
+        document.getElementById("download").addEventListener("click", (function() {snapshotPopup.recordDownload(chrome.extension.getBackgroundPage().snapshot.changeLogRevision)}), false);
+        document.getElementById("refresh").addEventListener("click", (function() {snapshotPopup.refresh()}), false);
+    });
+})();
