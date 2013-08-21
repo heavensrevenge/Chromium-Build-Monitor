a0ccf1492e7d2b209e16f6a2b6a42ed9e7de7b2e
Eric <ultimate.evil@gmail.com>
2013-08-21 04:53:23 -0400

    Removed null

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,5 +1,4 @@
 (function(){
-    var snapshotPopup = null;
     goog.events.listen(window, 'load', function() {
         snapshotPopup = new mbrio.SnapshotPopup();
         document.getElementById("download").addEventListener("click", (function() {snapshotPopup.recordDownload(chrome.extension.getBackgroundPage().snapshot.changeLogRevision)}), false);
