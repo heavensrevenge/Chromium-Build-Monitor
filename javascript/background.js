03e7f0bea68592c98438e888a268f2e5046503a9
Eric <ultimate.evil@gmail.com>
2013-08-24 03:54:26 -0400

    Update background.js

--- javascript/background.js
+++ javascript/background.js
@@ -12,7 +12,7 @@
 //
 
 (function(){
-	goog.events.listen(window, 'load', function() {
+	goog.events.listen(window, 'load', (function() {
 		snapshot = new mbrio.ChromiumSnapshot();
-	});
+	}));
 })();
