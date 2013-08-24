6ef14d708b7a28bdf17aaa6ec2a4f57f95a29d82
Eric <ultimate.evil@gmail.com>
2013-08-24 03:53:52 -0400

    Update options.js

--- javascript/options.js
+++ javascript/options.js
@@ -12,9 +12,13 @@
 //
 
 (function(){
-    goog.events.listen(window, 'load', function() {
-        optionsPage = new mbrio.OptionsPage();
-        document.getElementById("platform").addEventListener("change", function() {optionsPage.platformUpdated();}, false);
-        document.getElementById("saveButton").addEventListener("click", function() {optionsPage.saveOptions();}, false);
-    });
+	goog.events.listen(window, 'load', (function() {
+		optionsPage = new mbrio.OptionsPage();
+		document.getElementById("platform").addEventListener("change", (function() {
+			optionsPage.platformUpdated();
+		}));
+		document.getElementById("saveButton").addEventListener("click", (function() {
+			optionsPage.saveOptions();
+		}));
+	}));
 })();
