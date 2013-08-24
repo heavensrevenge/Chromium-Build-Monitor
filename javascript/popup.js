3d7f9511c41332f33c8b7deba487289973c8b60c
Eric <ultimate.evil@gmail.com>
2013-08-24 03:50:52 -0400

    Update popup.js

--- javascript/popup.js
+++ javascript/popup.js
@@ -12,11 +12,13 @@
 //
 
 (function(){
-    goog.events.listen(window, 'load', function() {
-        snapshotPopup = new mbrio.SnapshotPopup();
-        document.getElementById("download").addEventListener("click", function() {
-            snapshotPopup.recordDownload(chrome.extension.getBackgroundPage().snapshot.changeLogRevision);
-        }, false);
-        document.getElementById("refresh").addEventListener("click", function() {snapshotPopup.refresh();}, false);
-    });
+	goog.events.listen(window, 'load', (function() {
+		snapshotPopup = new mbrio.SnapshotPopup();
+		document.getElementById("download").addEventListener("click", (function() {
+			snapshotPopup.recordDownload(chrome.extension.getBackgroundPage().snapshot.changeLogRevision);
+		}));
+		document.getElementById("refresh").addEventListener("click", (function() {
+			snapshotPopup.refresh();
+		}));
+	}));
 })();
